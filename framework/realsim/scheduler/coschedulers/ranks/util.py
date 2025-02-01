62084c6edd4898295a53df951dce33a4ed40d77a
nikosT <ntriantafyl@cslab.ece.ntua.gr>
2025-02-02 00:36:50 +0200

    add util mech

--- framework/realsim/scheduler/coschedulers/ranks/util.py
+++ framework/realsim/scheduler/coschedulers/ranks/util.py
@@ -8,6 +8,7 @@ import sys
 from realsim.jobs.utils import deepcopy_list
 from itertools import combinations, chain
 from numpy import mean
+from math import ceil
 
 sys.path.append(os.path.abspath(os.path.join(
     os.path.dirname(__file__), "../../../../"
@@ -31,115 +32,32 @@ class UtilCoscheduler(RanksCoscheduler, ABC):
         """Condition on how to sort the hosts based on the speedup that the job
         will gain/lose. Always spread first
         """
-
-        # get the jobs signatures that are assinged to the host
-        co_job_sigs = list(self.cluster.hosts[hostname].jobs.keys())
-
-        # If no signatures then spread
-        if co_job_sigs == []:
-            return (job.max_speedup, inf)
-
-        # get average speedup for each job in host + candidate job
-        speedup = list(map(lambda j: self.database.heatmap[job.job_name][j.split(":")[-1]],co_job_sigs))
-        speedup += list(map(lambda j: self.database.heatmap[j.split(":")[-1]][job.job_name],co_job_sigs))
-        avg_speedup = sum(speedup) / (len(co_job_sigs)*2)
-
-        # get how many of the speedup values of pairs job,x and x,job for each x in host are bellow threshold
-        jobs_with_speedup = list(filter(lambda j: self.database.heatmap[job.job_name][j.split(":")[-1]] >= 1,co_job_sigs))
-        jobs_with_speedup += list(filter(lambda j: self.database.heatmap[j.split(":")[-1]][job.job_name] >= 1,co_job_sigs))
-        speedup_counts = len(jobs_with_speedup)
-
-        return (avg_speedup, speedup_counts)
-
-    def deploy(self) -> bool:
-        # it uses the "waiting_queue_reorder"
-        # it uses the "allocation" which uses the "host_alloc_condition"
-        def gen_subheatmaps(ids, depth=3):
-            combs = map(lambda i: combinations(ids, i), range(len(ids), len(ids)-depth, -1))
-            all_combs = chain.from_iterable(combs)
-            return list(all_combs)
+        def get_job(name: str, jobs: list) -> Job:
+            return next((j for j in jobs if j.job_name == name), None)
             
-        def sf(values, k=1):
-            return round(len(list(filter(lambda x: x > k, values)))/len(values), 2)
-            
-        def get_job(id: int, jobs: list) -> Job:
-            return next((j for j in jobs if j.job_id == id), None)
-        
         def speedup_score(j1: Job, j2: Job, compact=False) -> float:
             area1 = j1.num_of_processes * j1.remaining_time
             area2 = j2.num_of_processes * j2.remaining_time
             if compact:
                 return 1
             return (area1 * self.database.heatmap[j1.job_name][j2.job_name] +\
-                    area2 * self.database.heatmap[j2.job_name][j1.job_name]) / (area1 + area2)
+                    area2 * self.database.heatmap[j2.job_name][j1.job_name]) / (area1 + area2)        
 
-        def calc_values(co_schedule_ids, heatmap_list):
-            values = []
-            for id in co_schedule_ids:
-                values.append(speedup_score(get_job(id, heatmap_list), get_job(id, heatmap_list)))
-            values += [1]*(len(heatmap_list)-len(co_schedule_ids))
-            return values
-            
-        self.queue_depth = 10
+        # get the jobs signatures that are assinged to the host
+        co_job_sigs = list(self.cluster.hosts[hostname].jobs.keys())
 
-        waiting_queue = deepcopy_list(self.cluster.waiting_queue[:self.queue_depth])
-        execution_list = deepcopy_list(self.cluster.execution_list)
-        heatmap_list = waiting_queue + execution_list
+        host_jobs = list(map(lambda sig: get_job(sig.split(":")[-1], self.cluster.execution_list), co_job_sigs))
 
-        waiting_ids = list(map(lambda j: j.job_id, waiting_queue))
-        execution_ids = list(map(lambda j: j.job_id, execution_list))
-        heatmap_ids = waiting_ids + execution_ids
+        if host_jobs:
+            return min(list(map(lambda j: speedup_score(job, j),host_jobs)))
+        else:
+            return -inf
+        # If no signatures then spread
+        #if co_job_sigs == []:
+        #    return (job.max_speedup, inf)
 
-        # find all subheatmaps to the maximum depth of k
-        sub_heatmaps = gen_subheatmaps(heatmap_ids, depth=int(len(heatmap_ids)*0.5))
-
-        scores = []
-        for sh_ids in sub_heatmaps:
-            pairs = set(combinations(sh_ids, 2)) - set(combinations(execution_ids, 2))
-
-            values = []
-            for (id1, id2) in pairs:
-                values.append(speedup_score(get_job(id1, heatmap_list), get_job(id2, heatmap_list)))
-            values += [1]*(len(heatmap_list)-len(sh_ids))
-
-            scores.append((mean(values), sh_ids))
-        scores.sort(key=lambda x: -x[0])
-
-        # 0. if old, you have to deploy it
-        # 1. compact vs co-sched
-        # a. get wait and exec queue
-        # b. create all possible subheatmaps (depth=3) wait+exec * wait+exec
-        # c. calc sf keep the best
-        # d. job can now be scheduled as compact or co-sched
-
-        # 2. how to co-sched
-        #   a. best fit (utilization)
-        
-        deployed = False
-
-        # Update the rank of each job before scheduling them
-        # self.update_ranks()
-
-        while waiting_queue != []:
-
-            # Remove from the waiting queue
-            job = self.pop(waiting_queue)
-
-            if job.job_id in scores[0][1]:
-                policy=self.cluster.half_socket_allocation
-            else:
-                policy=self.cluster.full_socket_allocation 
-            # Colocate
-            if self.allocation(job, policy):
-                deployed = True
-                self.after_deployment()
-            else:
-                break
-
-        return deployed
-
-
-        #return super().deploy()
+    def deploy(self) -> bool:
+        return super().deploy()
 
     def backfill(self) -> bool:
         return super().backfill()
