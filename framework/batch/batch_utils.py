a56bd71e87250647fd4b99c2cbb61e614390917f
nikosT <ntriantafyl@cslab.ece.ntua.gr>
2025-04-01 19:40:47 +0300

    Add placeholder for new mighty ordered

--- framework/batch/batch_utils.py
+++ framework/batch/batch_utils.py
@@ -51,6 +51,7 @@ from realsim.scheduler.coschedulers.ranks.util3 import UtilCoscheduler3
 from realsim.scheduler.coschedulers.ranks.util_age import UtilCoscheduler_age
 from realsim.scheduler.coschedulers.ranks.util_frag import UtilCoscheduler_frag
 from realsim.scheduler.coschedulers.ranks.mighty import MightyCoscheduler
+from realsim.scheduler.coschedulers.ranks.mighty_ordered import MightyOrderedCoscheduler
 from realsim.scheduler.coschedulers.ranks.filler import FillerCoscheduler
 
 # Logger
